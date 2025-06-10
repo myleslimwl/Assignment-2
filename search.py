da1a24aa195364c5cc0aff361ac474f59770b53b
Tisha-1 <86545968+Tisha-1@users.noreply.github.com>
2025-06-10 18:23:05 +0800

    Update search.py
    -Added to function from start to end covering all treasures
    -Updated UCS code

--- search.py
+++ search.py
@@ -1,37 +1,61 @@
-#STILL HAVE ERROR#
-#------------------------Uniform-Cost Search Algorithm ------------------- #
-def uniformCostSearch(problem):
-    #Search node for least total cost
-    nodes = PriorityQueue()
-    fringe = []
-    path = []
-    explored = set([])
-    priority = 0
-    dict = {}
-    start_node = problem.GetStartState()
-    if problem.isGoalState(start_node):
-        return 'Already in goal'
-    else:
-        nodes.push((start_node, path), priority)
-        dict[start_node] = 0
-        explored.add(start_node)
-        while (nodes):
-            curr, path = nodes.pop()
+#---------------------Function to visit all treasures-----------------------#
+def find_best_treasure_path(start_pos, treasures):
+    best_path = []
+    min_total_cost = float('inf')
 
-            if problem.isGoalState(curr):
-                return path
-            else:
-                next = problem.getSuccessors(curr)
-                for node in nodes.heap:
-                    fringe.append(node[0])
-                for states in next:
-                    if states[0] not in (key for key in dict):
-                        cost = problem.getCostofActions(path + [states[1]])
-                        nodes.push((states[0], path + [states[1]]), cost)
-                        dict[states[0]] = cost
-                        explored.add(states[0])
-                    elif states[0] in (key for key in dict) and (problem.getCostofActoins(path + [states[1]]) < dict[states[0]]):
-                        cost = problem.getCostofActions(path + [states[1]])
-                        nodes.push((states[0], path + [states[1]]), cost)
-                        dict[states[0]] = cost
-                        explored.add(states[0])
+    for order in permutations(treasures):
+        current_pos = start_pos
+        total_path = []
+        total_cost = 0
+        valid = True
+
+        for target in order:
+            path_segment, cost = ucs(current_pos, target)
+            if not path_segment:
+                valid = False
+                break
+            total_path += path_segment
+            total_cost += cost
+            current_pos = target
+
+        if valid and total_cost < min_total_cost:
+            min_total_cost = total_cost
+            best_path = total_path
+
+    return best_path
+
+#-------------------------------Uniform-Cost Search Algorithm----------------------------#
+def ucs(start, goal):
+    #(cost, position)
+    open_set = [(0, start)]
+    came_from = {}
+    cost_so_far = {start: 0}
+
+    while open_set:
+        current_cost, current = heapq.heappop(open_set)
+
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.reverse()
+            return path, cost_so_far[goal]
+        
+        for neighbor in get_neighbors(*current):
+            tile = grid[neighbor[0]][neighbor[1]]
+            #Base minmax cost
+            step_cost = 1.0
+
+            if tile == 'X1' or tile == 'X2':
+                step_cost *= 2
+            elif tile == 'R1' or tile == 'R2':
+                step_cost *= 0.5
+
+            new_cost = current_cost + step_cost
+            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                cost_so_far[neighbor] = new_cost
+                came_from[neighbor] = current
+                heapq.heappush(open_set, (new_cost, neighbor))
+
+    return [], float('inf')
